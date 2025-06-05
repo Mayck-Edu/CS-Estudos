c4a977cfd0886770bb6bbf46aaf411698e1e6571
Mayck Eduardo <mayck_22.03@hotmail.com>
2025-06-05 11:17:18 -0300

    atualiza .gitignore para incluir novos padrões de exclusão e remove arquivos binários do projeto Starter

--- Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89e86a8017ff3cad16acd6a400968432b81b4a74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3510681bb50af75e397520f6296f490ca00c01f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
