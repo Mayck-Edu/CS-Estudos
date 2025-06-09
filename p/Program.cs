a1909617010ef74c7806531340f665e423be206c
Mayck Eduardo <mayck_22.03@hotmail.com>
2025-06-09 08:11:25 -0300

    ok

---
+++ p/Program.cs
@@ -0,0 +1,5 @@
+﻿string[] students = new string[] { "Alice", "Bob", "Charlie", "David" };
+
+int studentCount = students.Length;
+
+Console.WriteLine("The final name is: " + students[studentCount-1]);
