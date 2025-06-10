79de45bb4e5e9cc63b1b83fa2e0ab2f57c3c2f43
Mayck Eduardo <mayck_22.03@hotmail.com>
2025-06-10 16:36:49 -0300

    Adiciona implementação do serviço de boas-vindas e refatora o mapeamento de rotas

--- testes/WebApplication1/Program.cs
+++ testes/WebApplication1/Program.cs
@@ -1,17 +1,11 @@
-using Microsoft.AspNetCore.Rewrite;
+using MyWebApp.Interfaces;
+using MyWebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<IWelcomeService, WelcomeService>();
+
 var app = builder.Build();
 
-app.Use(async (context, next) =>
-{
-    Console.WriteLine($"{context.Request.Method} {context.Request.Path} {context.Response.StatusCode}");
-    await next(); 
-});
-
-app.UseRewriter(new RewriteOptions().AddRedirect("history", "about"));
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/about", () => "Contoso was founded in 2000.");
+app.MapGet("/", (IWelcomeService welcomeService) => welcomeService.GetWelcomeMessage());
 
 app.Run();
