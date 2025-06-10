14b5aab035daa9ca440934d51d869f32408ed6ad
Mayck Eduardo <mayck_22.03@hotmail.com>
2025-06-10 08:52:43 -0300

    Remove arquivos de configuração do Visual Studio e atualiza o SolutionGuid

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+int num1;
+int num2;
+
+// operacao();
+Console.WriteLine("Resultado: " + operacao());
+int operacao()
+{
+    Console.WriteLine("Digite o primeiro número:");
+    num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo número:");
+    num2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Escolha a operação: 1 - Soma, 2 - Subtração, 3 - Multiplicação, 4 - Divisão");
+    int operacao = Convert.ToInt32(Console.ReadLine());
+
+    switch (operacao)
+    {
+        case 1:
+            return num1 + num2;
+        case 2:
+            return num1 - num2;
+        case 3:
+            return num1 * num2;
+        case 4:
+            if (num2 != 0)
+                return num1 / num2;
+            else
+                throw new DivideByZeroException("Divisão por zero não é permitida.");
+        default:
+            throw new InvalidOperationException("Operação inválida.");
+    }
+}
